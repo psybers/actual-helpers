55e404cf5c9011bfb24da5e365f8f5cf598d938f
Robert Dyer <rdyer@unl.edu>
2024-07-21 22:05:15 -0500

    fix crash

--- zestimate.js
+++ zestimate.js
@@ -4,8 +4,9 @@ const { closeBudget, ensurePayee, getAccountBalance, getAccountNote, openBudget,
 require("dotenv").config();
 
 async function getZestimate(URL) {
+  let response = undefined;
   try {
-    const response = await fetch(URL, {
+    response = await fetch(URL, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
